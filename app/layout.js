0f1415d510d1841e72decc980f37d957b45bd7ce
Pujaksh-Design-Hub-Coaching <madhavkumar200625@gmail.com>
2026-01-17 23:55:54 +0530

    test

--- app/layout.js
+++ app/layout.js
@@ -114,7 +114,7 @@ export default function RootLayout({ children }) {
                 }
               `}
         </Script>
-          <div id="top-loader"></div>
+        <div id="top-loader"></div>
 
         <GlobalLinkLoader />
 
